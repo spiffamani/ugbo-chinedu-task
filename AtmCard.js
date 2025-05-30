06aeb3d44e56b7eeb947cb15175cb585b4394ab2
spiffamani <cjdaveeds@gmail.com>
2025-05-30 12:24:49 +0100

    Atm card program

---
+++ AtmCard.js
@@ -0,0 +1,40 @@
+
+function validateCardLength(user, cardNumber) {
+  const cardLengths = {
+    Visa: 16,
+    MasterCard: 16,
+    Discover: 16,
+    AmericanExpress: 15
+  };
+  const expectedLength = cardLengths[user];
+  if (cardNumber.length === expectedLength) {
+    return ( valid: true, valid );
+  }
+  return ( valid: false, Invalid Length );
+}
+
+  if (cardNumber || cardNumber.length === 0) {
+    return { valid: false, message: "Invalid card number" };
+  }
+
+switch (firstDigit) {
+    case 4:
+      return validateCardLength("Visa", cardNumber);
+    case 5:
+      return validateCardLength("MasterCard", cardNumber);
+    case 6:
+      return validateCardLength("Discover", cardNumber);
+    case 3:
+      return validateCardLength("AmericanExpress", cardNumber);
+    default:
+      return { valid: false, message: "Unknown card issuer" };
+  }
+}
+
+console.log("Here is my  card number:", validateCardLength([4, 5, 6, 2, 4, 5, 7, 8, 9, 2, 1, 4, 5, 6, 7, 7]))
+
+
+
+
+
+
